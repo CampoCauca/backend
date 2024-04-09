d2706f84f36eb6c855d4af7aad12be3e09142edc
Daniel Eduardo Rojas Cortés <daniel.rojas@sunsetswh.com>
2024-04-09 02:39:57 -0500

    Se ajusta bug en ArticuloSerializer, se estaba declarando mal el model

--- modules/product/serializers.py
+++ modules/product/serializers.py
@@ -2,14 +2,7 @@ from rest_framework import serializers
 from administracion.models import Articulo
 
 
-class ArticuloSerializer(serializers.HyperlinkedModelSerializer):
+class ArticuloSerializer(serializers.ModelSerializer):
     class Meta:
-        model: Articulo
-        fields = (
-            "id_articulo",
-            "nombre_articulo",
-            "descripcion_articulo",
-            "imagen",
-            "categoria_id_categoria",
-            "unidad_de_medida_idunidad_de_medida",
-        )
+        model = Articulo
+        fields = "__all__"
