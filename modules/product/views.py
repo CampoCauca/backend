dffc1350165d4ea287624cf6bb2be2ac97037df0
Daniel Eduardo Rojas Cortés <daniel.rojas@sunsetswh.com>
2024-05-11 03:42:19 -0500

    Se agrega documentación para servicio de Articulos en swagger

--- modules/product/views.py
+++ modules/product/views.py
@@ -1,4 +1,5 @@
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from administracion.models import Articulo
 from rest_framework import viewsets, permissions
@@ -19,6 +20,19 @@ class ArticuloViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(persona_id_persona=id_persona)
 
         return queryset
+    
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "idPersona",
+                openapi.IN_QUERY,
+                description="ID of the producto",
+                type=openapi.TYPE_INTEGER,
+            ),
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(auto_schema=None)
     def partial_update(self, request, pk, *args, **kwargs):
